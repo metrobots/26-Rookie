a55906e04aeffae077422d09281cc84be8f2a4fd
KennethIsTaken <braekevelt-nichols.1@themetroschool.org>
2026-02-09 17:51:52 -0500

    Import libraries for climb

--- src/main/java/frc/robot/subsystems/Climber/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber/Climber.java
@@ -1,1 +1,20 @@
-//Not a thought going through that head. Brain is empty, save for that dumbfounded look on his face.
+// Need: motor. moves smaller tube into larger tube, MUST be bound to "B" button
+// What motor does: talk with climb about this
+
+// checklist:
+  // Import needed libraries
+  // ID a motor
+  // Make it motion
+  // ID Xbox controller
+  // Bind it to "B"
+
+package frc.robot.subsystems.Climber;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
+import edu.wpi.first.wpilibj.XboxController;
+
+
+public class Climber extends SubsystemBase {
+
+}
