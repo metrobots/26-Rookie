ad98c070c4371e99dde2df48651ab5958c3b8c01
KennethIsTaken <braekevelt-nichols.1@themetroschool.org>
2026-02-11 17:33:10 -0500

    Added motor + controller (Untested)
    This code needs to be tested. If at first you don't succeed, cry, cry again.

--- src/main/java/frc/robot/subsystems/Climber/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber/Climber.java
@@ -1,12 +1,20 @@
 // Need: motor. moves smaller tube into larger tube, MUST be bound to "B" button
-// What motor does: talk with climb about this
+// What motor does: Winch moving motor, extends when B pressed, retracts when B pressed again.
+
+// V V V V V V V
+// Conduct testing on a motor.
+// Ask Evergreen how to test the motor.
+// If it doesn't work, scream internally, then troubleshoot.
+// ^ ^ ^ ^ ^ ^ ^
+
+
 
 // checklist:
-  // Import needed libraries
-  // ID a motor
-  // Make it motion
-  // ID Xbox controller
-  // Bind it to "B"
+  // Import needed libraries - done 
+  // ID a motor - done
+  // Make it motion - needs testing
+  // ID Xbox controller - done
+  // Bind it to "B" - needs testing
 
 package frc.robot.subsystems.Climber;
 
@@ -15,6 +23,21 @@ import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
 import edu.wpi.first.wpilibj.XboxController;
 
 
-public class Climber extends SubsystemBase {
 
+public class Climber extends SubsystemBase {
+  
+  private final PWMSparkMax climberMotor = new PWMSparkMax(0); // Assuming channel 0 for the motor
+  private final XboxController controller = new XboxController(1); // Assuming controller is on port 1
+
+ public boolean getBButtonPressed() {
+    return controller.getBButton(); // Returns true if B button is pressed
+  }
+
+  public void controlClimber() {
+    if (getBButtonPressed()) {
+      climberMotor.set(1.0); // Extend the climber
+    } else {
+      climberMotor.set(-1.0); // reverse the motor when B is not pressed
+    }
+  }
 }
