aa4ab82f9e5b8ae5a13e6bf1a6e93ae2a3413f3c
KennethIsTaken <braekevelt-nichols.1@themetroschool.org>
2026-02-14 10:51:05 -0500

    Added a limit* to the motor's function** so it doesn't kill the bot***
    *Tried to add a limit. it still need to be tested
    **The motor's speed
    ***If the motor keeps trying to run when it's meant to be stopped, it could cause unnecessary wear or damage to the bot systems

--- src/main/java/frc/robot/subsystems/Climber/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber/Climber.java
@@ -1,20 +1,18 @@
 // Need: motor. moves smaller tube into larger tube, MUST be bound to "B" button
-// What motor does: Winch moving motor, extends when B pressed, retracts when B pressed again.
+// What motor does: motor running a winch, extends when B pressed, retracts when not.
 
 // V V V V V V V
 // Conduct testing on a motor.
-// Ask Evergreen how to test the motor.
-// If it doesn't work, scream internally, then troubleshoot.
 // ^ ^ ^ ^ ^ ^ ^
 
-
-
 // checklist:
-  // Import needed libraries - done 
-  // ID a motor - done
-  // Make it motion - needs testing
-  // ID Xbox controller - done
-  // Bind it to "B" - needs testing
+  // 1. Import needed libraries - done 
+  // 2. ID a motor - done
+  // 3. Make it motion - needs testing
+  // 4. ID Xbox controller - done
+  // 5. Bind it to "B" - needs testing
+  // 6. OHH NOO I NEED TO ADD LIMITS OR IT'LL RIP THE ROBOT APART FROM THE INSIDE OUT - that's rough bro. good luck.
+  // 7. I think i did #6 - needs testing
 
 package frc.robot.subsystems.Climber;
 
@@ -22,12 +20,12 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
 import edu.wpi.first.wpilibj.XboxController;
 
-
-
 public class Climber extends SubsystemBase {
   
   private final PWMSparkMax climberMotor = new PWMSparkMax(0); // Assuming channel 0 for the motor
   private final XboxController controller = new XboxController(1); // Assuming controller is on port 1
+
+  int climberLimit = 0;
 
  public boolean getBButtonPressed() {
     return controller.getBButton(); // Returns true if B button is pressed
@@ -35,9 +33,18 @@ public class Climber extends SubsystemBase {
 
   public void controlClimber() {
     if (getBButtonPressed()) {
-      climberMotor.set(1.0); // Extend the climber
-    } else {
-      climberMotor.set(-1.0); // reverse the motor when B is not pressed
+      climberMotor.set(0.5); // Extend the climber
+      climberLimit = climberLimit + 1;
+      while (getBButtonPressed() && climberLimit >= 100) {
+        climberMotor.set(0);
+      }
+    } 
+    else {
+      climberMotor.set(-0.5); // reverse the motor when B is not pressed
+      climberLimit = climberLimit - 1; 
+      while (!getBButtonPressed() && climberLimit <= 0) {
+      climberMotor.set(0);
+      }
     }
   }
 }
